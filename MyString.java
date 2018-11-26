d297df1f84ba9c4c6f79c862b6ddc498b6aa4fe8
Aditi Haiman <aditi@aditis-air.lan>
2018-11-26 09:31:22 -0500

    fixed compareTo based on testing

--- MyString.java
+++ MyString.java
@@ -26,6 +26,88 @@ public class MyString implements CharSequence, Comparable<CharSequence> {
   //   System.out.println("world".compareTo("world"));
   //
   // }
+  public static void main(String[] args) {
+
+    // sole constructor test
+    MyString msg = new MyString("droid");
+    System.out.println("MyString msg = new MyString(\"charlotte\")");
+
+    // toString()
+    System.out.println("msg: " + msg);                                                  // "droid"
+
+    // length()
+    System.out.println("msg.length(): " + msg.length());                                // 5
+
+    System.out.println();
+
+    // charAt() exception catch
+    System.out.println("msg.charAt(-1):");
+    try {
+      System.out.println(msg.charAt(-1));                             // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    System.out.println("msg.charAt(15):");
+    try {
+      System.out.println(msg.charAt(15));                             // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // charAt()
+    System.out.println("msg.charAt(0): " + msg.charAt(0));                              // "d"
+    System.out.println("msg.charAt(3): " + msg.charAt(3));                              // "i"
+
+    System.out.println();
+
+    // subSequence() exception catch
+    // start < 0
+    System.out.println("msg.subSequence(-1,4):");
+    try {
+      System.out.println(msg.subSequence(-1,4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // end < 0
+    System.out.println("msg.subSequence(1,-4):");
+    try {
+      System.out.println(msg.subSequence(1,-4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // start > end
+    System.out.println("msg.subSequence(11,4):");
+    try {
+      System.out.println(msg.subSequence(11,4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // end > length()
+    System.out.println("msg.subSequence(1,14):");
+    try {
+      System.out.println(msg.subSequence(1,14));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // subSequence()
+    System.out.println("msg.subSequence(0,3): " + msg.subSequence(0,3));                // "dro"
+    System.out.println("msg.subSequence(2,5): " + msg.subSequence(2,5));                // "oid"
+
+    System.out.println();
+
+    // compareTo()
+    System.out.println("msg.compareTo(\"droid\"): " + msg.compareTo("droid"));          // 0
+    System.out.println("msg.compareTo(\"bat\"): " + msg.compareTo("bat"));              // 1
+    System.out.println("msg.compareTo(\"seventh\"): " + msg.compareTo("seventh"));      // -1
+    System.out.println("msg.compareTo(\"drank\"): " + msg.compareTo("drank"));          // 1
+    System.out.println("msg.compareTo(\"drunk\"): " + msg.compareTo("drunk"));          // -1
+
+  }
   private char[] charArray;
 
 //—————————————————————————————————CONSTRUCTORS—————————————————————————————————————————//
@@ -71,16 +153,19 @@ public class MyString implements CharSequence, Comparable<CharSequence> {
 
   public int compareTo(CharSequence obj) {
     int output = 0;
+    boolean check = true;
     if (obj.equals(null)) throw new NullPointerException();
-    for (int x = 0; x < obj.length() && x < this.length(); x++) { //checks equality of chars up to the length of shorter CharSequence
+    for (int x = 0; check && x < obj.length() && x < this.length(); x++) { //checks equality of chars up to the length of shorter CharSequence
       if (this.charAt(x)== obj.charAt(x)) output = 0;
       else {
-        if ((this.charAt(x)) > (obj.charAt(x))) output = 1;
-        output = -1;
+        //System.out.println(this.charAt(x) + " ; " + obj.charAt(x));
+        output = this.charAt(x) - obj.charAt(x);
+        check = false; //if the code reaches this point, it should exit the loop
       }
     }
-    if (output == 0 && this.length() > obj.length()) output = 1; //checks if the two CharSequences have different lengths
-    if (output == 0 && this.length() < obj.length()) output = -1;
+    if (output == 0 && this.length() != obj.length()) { //checks if the two CharSequences have different lengths
+      output = this.length() - obj.length();
+    }
     return output;
   }
 
