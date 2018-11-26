c63d7c7d6e9cb12b21dce6700eb3d705673c1d6a
Aditi Haiman <aditi@aditis-air.lan>
2018-11-26 09:29:01 -0500

    tested built in compare method

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+  public static void main(String[] args) {
+    System.out.println('a' - 'C');
+    System.out.println("a".compareTo("aF"));
+    System.out.println("aF".compareTo("a"));
+    System.out.println("droid".compareTo("bat"));
+
+  }
+}
