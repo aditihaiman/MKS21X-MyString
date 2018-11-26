938c32b5da9db1b3b7701db897a134243fd0ffa9
Aditi Haiman <aditi@aditis-air.lan>
2018-11-26 09:33:42 -0500

    final testing

--- MyString.java
+++ MyString.java
@@ -1,113 +1,35 @@
 public class MyString implements CharSequence, Comparable<CharSequence> {
-  // public static void main(String[] args) {
-  //   MyString str = new MyString("hello");
-  //   MyString str1 = new MyString("world");
-  //   MyString str2 = new MyString("fish");
-  //
-  //   System.out.println(str.charAt(4));
-  //   System.out.println("hello".charAt(4));
-  //   System.out.println();
-  //   System.out.println(str2.length());
-  //   System.out.println("fish".length());
-  //   System.out.println();
-  //   System.out.println(str2.subSequence(0, 2));
-  //   System.out.println("fish".substring(0, 2));
-  //   System.out.println();
-  //   System.out.println(str.toString());
-  //   System.out.println("hello".toString());
-  //   System.out.println();
-  //   System.out.println(str.compareTo("worlds"));
-  //   System.out.println("hello".compareTo("worlds"));
-  //   System.out.println();
-  //   System.out.println(str1.compareTo("worl"));
-  //   System.out.println("world".compareTo("worl"));
-  //   System.out.println();
-  //   System.out.println(str1.compareTo("world"));
-  //   System.out.println("world".compareTo("world"));
-  //
-  // }
   public static void main(String[] args) {
+    MyString str = new MyString("hello");
+    MyString str1 = new MyString("world");
+    MyString str2 = new MyString("fish");
 
-    // sole constructor test
-    MyString msg = new MyString("droid");
-    System.out.println("MyString msg = new MyString(\"charlotte\")");
-
-    // toString()
-    System.out.println("msg: " + msg);                                                  // "droid"
-
-    // length()
-    System.out.println("msg.length(): " + msg.length());                                // 5
-
+    System.out.println(str.charAt(4));
+    System.out.println("hello".charAt(4));
     System.out.println();
-
-    // charAt() exception catch
-    System.out.println("msg.charAt(-1):");
-    try {
-      System.out.println(msg.charAt(-1));                             // IndexOutOfBoundsException
-    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
-
+    System.out.println(str2.length());
+    System.out.println("fish".length());
     System.out.println();
-
-    System.out.println("msg.charAt(15):");
-    try {
-      System.out.println(msg.charAt(15));                             // IndexOutOfBoundsException
-    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
-
+    System.out.println(str2.subSequence(0, 2));
+    System.out.println("fish".substring(0, 2));
     System.out.println();
-
-    // charAt()
-    System.out.println("msg.charAt(0): " + msg.charAt(0));                              // "d"
-    System.out.println("msg.charAt(3): " + msg.charAt(3));                              // "i"
-
+    System.out.println(str.toString());
+    System.out.println("hello".toString());
     System.out.println();
-
-    // subSequence() exception catch
-    // start < 0
-    System.out.println("msg.subSequence(-1,4):");
-    try {
-      System.out.println(msg.subSequence(-1,4));                      // IndexOutOfBoundsException
-    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
-
+    System.out.println(str.compareTo("worlds"));
+    System.out.println("hello".compareTo("worlds"));
     System.out.println();
-
-    // end < 0
-    System.out.println("msg.subSequence(1,-4):");
-    try {
-      System.out.println(msg.subSequence(1,-4));                      // IndexOutOfBoundsException
-    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
-
+    System.out.println(str1.compareTo("worl"));
+    System.out.println("world".compareTo("worl"));
     System.out.println();
-
-    // start > end
-    System.out.println("msg.subSequence(11,4):");
-    try {
-      System.out.println(msg.subSequence(11,4));                      // IndexOutOfBoundsException
-    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
-
+    System.out.println(str1.compareTo("world"));
+    System.out.println("world".compareTo("world"));
     System.out.println();
-
-    // end > length()
-    System.out.println("msg.subSequence(1,14):");
-    try {
-      System.out.println(msg.subSequence(1,14));                      // IndexOutOfBoundsException
-    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
-
-    System.out.println();
-
-    // subSequence()
-    System.out.println("msg.subSequence(0,3): " + msg.subSequence(0,3));                // "dro"
-    System.out.println("msg.subSequence(2,5): " + msg.subSequence(2,5));                // "oid"
-
-    System.out.println();
-
-    // compareTo()
-    System.out.println("msg.compareTo(\"droid\"): " + msg.compareTo("droid"));          // 0
-    System.out.println("msg.compareTo(\"bat\"): " + msg.compareTo("bat"));              // 1
-    System.out.println("msg.compareTo(\"seventh\"): " + msg.compareTo("seventh"));      // -1
-    System.out.println("msg.compareTo(\"drank\"): " + msg.compareTo("drank"));          // 1
-    System.out.println("msg.compareTo(\"drunk\"): " + msg.compareTo("drunk"));          // -1
+    System.out.println(str2.compareTo("TUNAFISH"));
+    System.out.println("fish".compareTo("TUNAFISH"));
 
   }
+
   private char[] charArray;
 
 //—————————————————————————————————CONSTRUCTORS—————————————————————————————————————————//
